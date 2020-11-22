29568104da47482a78a34817f18d720844351a6c
pgm-cagravci <cagravci@student.arteveldehs.be>
2020-11-22 17:05:00 +0100

    javascript toegevoegd, klikken op hamburgericoon

---
+++ _static/js/main.js
@@ -0,0 +1,49 @@
+/* 
+Ailiv
+-------------------------
+Naam: Avci Cagri
+created: 15/11/2020
+*/
+
+(() => {
+    const app = {
+        initialize: function () {
+
+
+            //chashe html elements
+            this.casheElements();
+            //add click events 
+            this.registerListeners();
+
+        },
+        //show the navItems when the users clicks on the hamburgericon
+        showNav(){
+            let isHidden = false;
+
+            //find out if the navbar is hidden or not
+            let navItemsClass = this.$navItems.classList;
+            navItemsClass.forEach(classItem => {
+                if (classItem == "hidden") {
+                isHidden = true;
+                };
+            });
+            //if the navbar is hidden, the click needs to show the navbar. else is needs to hide is.
+            if (isHidden) {
+                this.$navItems.className = "nav_items";
+            } else{
+                this.$navItems.className = "nav_items hidden";
+
+            }
+        },
+        casheElements() {
+            this.$hamburger = document.querySelector(".hamburger-icon");
+            this.$navItems = document.querySelector(".nav_items");
+        },
+        registerListeners() {
+            this.$hamburger.addEventListener('click', (event) => {
+                this.showNav();
+            });
+        }
+    };
+    app.initialize();
+})();
