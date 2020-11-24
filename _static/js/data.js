af8e4ea3317ce41e13c76d7af885a1c733c7e82c
pgm-cagravci <cagravci@student.arteveldehs.be>
2020-11-24 13:36:14 +0100

    toevoegen van webshop items met js

---
+++ _static/js/data.js
@@ -0,0 +1,47 @@
+const products = [
+    {   
+        id: "0000",
+        name: "boeket",
+        type: "small",
+        price: {
+            value: 20,
+            currency: "EUR",
+            symbol: "€",
+        },
+        image: {
+            url: "../_static/media/images/flowers_webshop_small.png",
+            alt: "boeket small"
+        },
+
+    },
+    {   
+        id: "0001",
+        name: "boeket",
+        type: "medium",
+        price: {
+            value: 30,
+            currency: "EUR",
+            symbol: "€",
+        },
+        image: {
+            url: "../_static/media/images/flowers_webshop_medium.png",
+            alt: "boeket medium"
+        },
+
+    },
+    {   
+        id: "0002",
+        name: "boeket",
+        type: "large",
+        price: {
+            value: 50,
+            currency: "EUR",
+            symbol: "€",
+        },
+        image: {
+            url: "../_static/media/images/flowers_webshop_large.png",
+            alt: "boeket large"
+        },
+
+    },
+]
--- _static/js/main.js
+++ _static/js/main.js
@@ -5,17 +5,45 @@ Naam: Avci Cagri
 created: 15/11/2020
 */
 
+
 (() => {
+    
     const app = {
         initialize: function () {
 
-
+            this.createWebshopItems();
             //chashe html elements
             this.cacheElements();
             //add click events 
             this.registerListeners();
 
         },
+        createWebshopItems:function () {
+            //grab webshop element
+            let $cardContainer = document.querySelector(".webshop-cards")
+            console.log($cardContainer);
+           
+            let tempStr = "";
+
+            //make a section per product in data.js{products}
+            products.forEach(product => {
+                tempStr += `
+                <section class="card">
+                    <img src="${product.image.url}" alt="${product.image.alt}">
+                    <div class="card-info">
+                        <a href="../product/boeket/index.html">
+                            <h4>${product.name} - ${product.type}</h4>
+                        </a>
+                        <p class="price">${product.price.symbol}${product.price.value}</p>
+                    </div>
+                </section>
+                `
+            });
+            //add the created string to the innerHTML om the container
+            $cardContainer.innerHTML = tempStr;
+
+        },
+        //subfunctions:
         //show the navItems when the users clicks on the hamburgericon
         showNav(){
             let isHidden = false;
