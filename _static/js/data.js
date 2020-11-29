680f7c7e3c30d345bd0f067ba35d52385392792e
pgm-cagravci <cagravci@student.arteveldehs.be>
2020-11-29 04:14:30 +0100

    fixed the basket

--- _static/js/data.js
+++ _static/js/data.js
@@ -49,6 +49,96 @@ const products = [
 let seasons = [
     {
         season: "lente",
-        image: ""
+        description:"In de lente komt alles tot bloei",
+        image: [
+            "_static/media/images/flowers_lente.jpg"
+        ]
+    },
+    {
+        season: "Pasen",
+        description:"Paasbloemen",
+        image: [
+            "_static/media/images/flowers_pasen.jpg"
+        ]
+    },
+    {
+        season: "Zomer",
+        description:"Zomerbloemen",
+        image: [
+            "_static/media/images/flowers_zomer.jpg"
+        ]
+    },
+    {
+        season: "Kerst",
+        description:"",
+        image: [
+            "_static/media/images/flowers_kerst1.jpg",
+            "_static/media/images/flowers_kerst2.jpg",
+            "_static/media/images/flowers_kerst3.jpg",
+            "_static/media/images/flowers_kerst4.jpg",
+            "_static/media/images/flowers_kerst5.jpg",
+            "_static/media/images/flowers_kerst6.jpg"
+        ]
+    }
+]
+
+let examples = [
+    {
+        type: "season",
+        images: [
+            "_static/media/images/flowers_examples1.jpeg",
+            "_static/media/images/flowers_examples2.jpeg",
+            "_static/media/images/flowers_examples3.jpeg"
+        ]
+    },
+    {
+        type: "personalised",
+        images: [
+            "_static/media/images/flowers_examples4.png",
+            "_static/media/images/flowers_examples5.png",
+            "_static/media/images/flowers_examples6.png",
+            "_static/media/images/flowers_examples7.png",
+        ]
+    }
+]
+
+let basketOrders = [
+    {
+        productId: "0001",
+        amount:2,
+        color:"white",
+        vase:false,
+        binding:"short",
+        findOrder() {
+            let id =0;
+            products.forEach(product => {
+                if (product.id == this.productId) {
+                    id = products.indexOf(product);
+                    
+                }
+                
+            })
+            return id;
+        }
+
+    },
+    {
+        productId: "0001",
+        amount:2,
+        color:"white",
+        vase:false,
+        binding:"short",
+        findOrder() {
+            let id =0;
+            products.forEach(product => {
+                if (product.id == this.productId) {
+                    id = products.indexOf(product);
+                    
+                }
+                
+            })
+            return id;
+        }
+
     }
 ]
--- _static/js/main.js
+++ _static/js/main.js
@@ -15,6 +15,13 @@ created: 15/11/2020
             this.createWebshopItems();
             //show the selected product
             this.createProductPage();
+            //create the seasonal page
+            this.createSeasonsPage();
+            //create examples: container cards
+            this.createExamples();
+
+            //create the basket
+            this.createBasket();
 
             //scroll to top button
             this.createScrollTop();
@@ -134,8 +141,174 @@ created: 15/11/2020
             $body.innerHTML += str;
 
         },
-        showBasket($basket){
+        createSeasonsPage: function () {
+            let $season = document.querySelector(".season-card-container")
 
+            if ($season === null) {
+
+            }else {
+                this.populateSeasonsPage($season);
+            }
+        },
+        populateSeasonsPage: function ($season) {
+            let str = "";
+            
+            seasons.forEach(season => {
+                let imgStr = "";
+                season.image.forEach(imgUrl => {
+                    imgStr+= `
+                    <img src="${imgUrl}" alt="${season.season}">`
+                })
+
+                str += `
+                <section class="season-card">
+                <h2>${season.season}</h2>
+                ${imgStr}
+                <p>${season.description}</p>
+            </section>`
+            })
+            $season.innerHTML = str;
+        },
+        createExamples: function () {
+            let $examples = document.querySelector(".card--container.season-examples")
+
+            if ($examples === null) {
+
+            }else {
+                this.populateExamples($examples);
+            }
+        },
+        populateExamples: function ($examples) {
+            let str ="";
+            let exampleType = "";
+            let url = window.location.pathname
+            if (url == "/dienst_seizoensdecoratie_index.html"){
+                exampleType = "season";
+            }else if (url == "/dienst_gepersonaliseerd_bloemwerk_index.html") {
+                exampleType = "personalised"
+            } else {
+
+            }
+
+            examples.forEach(example => {
+                if (example.type == exampleType) {
+                    example.images.forEach(img => {
+                        str += `
+                        <section class="card">
+                         
+                         <a href="${img}" target="_blank"> 
+                             <img src="${img}" alt="${example.type}">
+                         </a>
+                     </section>`
+                    })
+               
+                }
+            })
+            $examples.innerHTML = str;
+
+        },
+        createBasket(){
+            this.$sidePanel = document.querySelector(".darken")
+            if (this.$sidePanel != null) {
+                this.$sidePanel.remove()
+            }
+            document.querySelector("header").innerHTML += `
+            <article class="darken hidden">
+            <div class="shopping-sidePanel">
+                <div class="shopping-top">
+                    <h2>uw winkelmandje</h2>
+                    <a id="close" href="#">
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    viewBox="0 0 11.328 11.328" style="enable-background:new 0 0 11.328 11.328;" xml:space="preserve">
+                <g>
+                    <path style="fill:#030104;" d="M11.145,9.085L7.723,5.664l3.422-3.421c0.243-0.244,0.243-0.64,0-0.883L9.967,0.183
+                        c-0.244-0.244-0.64-0.244-0.883,0L5.662,3.604l-3.42-3.421c-0.244-0.244-0.639-0.244-0.883,0L0.183,1.36
+                        c-0.243,0.244-0.243,0.639,0,0.883l3.421,3.421L0.183,9.085c-0.243,0.244-0.243,0.639,0,0.883l1.177,1.178
+                        c0.244,0.243,0.639,0.243,0.883,0l3.42-3.422l3.422,3.422c0.243,0.243,0.639,0.243,0.883,0l1.178-1.178
+                        C11.389,9.724,11.389,9.329,11.145,9.085z"/>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                        </svg>
+                    </a>
+                </div>
+                <section class="shopping-list">
+                    
+                </section>
+                <section class="shopping-bottom">
+                    <div class="shopping-bottom-total">
+                        <h4>Subtotal</h4>
+                        <p>€0</p>
+                    </div>
+                    <a class="shopping-bottom-action" href="#">Naar betalen</a>
+                </section>
+            </div>
+        </article>`
+
+        let str = "";
+        let $shopping = document.querySelector(".shopping-list")
+
+        let subTot = 0;
+
+        basketOrders.forEach(order => {
+            let orderedProduct = products[order.findOrder()] 
+            subTot = subTot + order.amount*orderedProduct.price.value
+            str += `
+                    <div class="shopping-product">
+                        <div class="shopping-product-image">
+                            <img src="${orderedProduct.image.url}" alt="">
+                        </div>
+                    
+                        <div class="shopping-product-info">
+                            <h4>${orderedProduct.name} ${orderedProduct.type}</h4>
+                            <p>${orderedProduct.price.symbol}${orderedProduct.price.value}</p>
+                            <p><span>Kleur</span>${order.color}</p>
+                            <p><span>Vaas bijbestellen</span>${order.vase}</p>
+                            <p><span>Bindwijze:</span>${order.binding}</p>
+                            <a href="#">verwijderen</a>
+                        </div>
+                        <div class="shopping-product-amount">
+                            <input type="text" name="number" id="number" min="0" value="${order.amount}">
+                        </div>
+                    </div>`
+        })
+
+            $shopping.innerHTML = str;
+            subTot = "€" + subTot;
+
+            document.querySelector(".shopping-bottom-total p").innerHTML = subTot;
+        },
+        showBasket() {
+            this.$sidePanel.className = "darken";
+            
         },
         //subfunctions:
         //show the navItems when the users clicks on the hamburgericon
@@ -166,6 +339,8 @@ created: 15/11/2020
             this.$navItems = document.querySelector(".nav_items");
             this.$btn = document.querySelector("#myBtn")
             this.$basket = document.querySelector(".nav__basket")
+            this.$close = document.querySelector("#close")
+            this.$sidePanel = document.querySelector(".darken")
         },
         registerListeners() {
             this.$hamburger.addEventListener('click', (event) => {
@@ -177,8 +352,13 @@ created: 15/11/2020
             })
 
             this.$basket.addEventListener('click', (event) => {
-                this.showBasket($basket);
+                this.showBasket();
             })
+
+            this.$close.addEventListener('click', (event) => {
+                this.$sidePanel.className = "darken hidden";
+            } )
+
         }
     };
     app.initialize();
